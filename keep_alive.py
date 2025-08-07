c9ad5ba37316a36eacf2e5bb4752946e512e0e4b
nazonikubto <moltor121252@gmail.com>
2025-08-07 21:49:03 +0900

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,19 +1,7 @@
 from flask import Flask
-from threading import Thread
 
-# Flaskアプリ作成
-app = Flask('')
+app = Flask(__name__)
 
-# ルートURLにアクセスしたときのレスポンス
 @app.route('/')
 def home():
-    return "I'm alive!"
-
-# Flaskアプリを別スレッドで実行
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-# keep_alive関数を呼ぶことでFlaskアプリが起動
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    return "Bot is running!"
