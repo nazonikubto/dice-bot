3c0c407877250f292a634d9bc6b7e2efaab12410
nazonikubto <moltor121252@gmail.com>
2025-08-08 22:41:03 +0900

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,19 @@
 # keep_alive.py
+
 from flask import Flask
+from threading import Thread
+from bot import start_bot  # bot.py に定義された起動関数
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running!"
+    return "Bot is running via gunicorn!"
+
+# サーバー起動時に Discord Bot を別スレッドで起動
+Thread(target=start_bot).start()
+
+
 
 
 
